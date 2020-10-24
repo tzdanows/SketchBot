5bcd315d0f5433f8ac76d5c1fc029d5374e5aa8f
Alex Rodriguez <arodr187@depaul.edu>
2020-10-24 03:11:17 -0500

    seperated consts to seperate file

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-
 require('dotenv').config();
 const Discord = require('discord.js');
 var Jimp = require('jimp');
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
+const data = require('./data');
 
 bot.login(TOKEN);
 
@@ -29,8 +29,8 @@ function draw(index, color, msg){
   const letter = index[0].toUpperCase();
   Jimp.read('resources/template.png', (err, template) => {
   if (err) throw err;
-  for (var x = numDict[number][0] ; x < numDict[number][1] ; x++){
-    for (var y = letterDict[letter][0] ; y < letterDict[letter][1] ; y++){
+  for (var x = data.numDict[number][0] ; x < data.numDict[number][1] ; x++){
+    for (var y = data.letterDict[letter][0] ; y < data.letterDict[letter][1] ; y++){
       template.setPixelColor(Jimp.cssColorToHex(color), x, y);
       }
     }
@@ -60,63 +60,3 @@ function sendMessage(msg){
   }); 
 }
 
-const letterDict = {
-  A: [21, 40], 
-  B: [41, 60],
-  C: [61, 80],
-  D: [81, 100],
-  E: [101, 120],
-  F: [121, 140],
-  G: [141, 160],
-  H: [161, 180],
-  I: [181, 200],
-  J: [201, 220],
-  K: [221, 240],
-  L: [241, 260],
-  M: [261, 280],
-  N: [281, 300],
-  O: [301, 320],
-  P: [321, 340],
-  Q: [341, 360],
-  R: [361, 380],
-  S: [381, 400],
-  T: [401, 420],
-  U: [421, 440],
-  V: [441, 460],
-  W: [461, 480],
-  X: [481, 500],
-};
-
-
-const numDict = {
-  1: [21, 40],
-  2: [41, 60],
-  3: [61, 80],
-  4: [81, 100],
-  5: [101, 120],
-  6: [121, 140],
-  7: [141, 160],
-  8: [161, 180],
-  9: [181, 200],
-  10: [201, 220],
-  11: [221, 240],
-  12: [241, 260],
-  13: [261, 280],
-  14: [281, 300],
-  15: [301, 320],
-  16: [321, 340],
-  17: [341, 360],
-  18: [361, 380],
-  19: [381, 400],
-  20: [401, 420],
-  21: [421, 440],
-  22: [441, 460],
-  23: [461, 480],
-  24: [481, 500],
-  25: [501, 520],
-  26: [521, 540],
-  27: [541, 560],
-  28: [561, 580],
-  29: [581, 600],
-  30: [601, 620],
-  };
