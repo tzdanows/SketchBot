86b3d1c78043c4a45ec455860e99524f2563a248
Alex Rodriguez <arodr187@depaul.edu>
2020-10-23 23:09:13 -0500

    renamed template, added argument logic

--- index.js
+++ index.js
@@ -11,30 +11,24 @@ bot.on('ready', () => {
 });
 
 bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-    msg.channel.send('pong', {
+  if (msg.content.startsWith('!sketch')) {
+    var args = (msg.content.trim().split(/ +/g)).slice(0);
+    args.shift();
+    console.log(args);
+    msg.channel.send('Heres the current sketch:', {
       files:[
-        "resources/template.png"
+        "resources/templateNew.png"
       ]
     });
-
-  } else if (msg.content.startsWith('!kick')) {
-    if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-    } else {
-      msg.reply('Please tag a valid user!');
-    }
-  }
+  } 
 });
 
-Jimp.read('resources/template.png', (err, template) => {
+Jimp.read('resources/templateNew.png', (err, template) => {
   if (err) throw err;
   for (var x = 21 ; x <= 620 ; x ++){
     for (var y = 21 ; y <= 500 ; y ++){
       if (x % 20 != 0 && y %20 != 0){
-      template.setPixelColor(Jimp.cssColorToHex('#ff800e'), x, y);
+      template.setPixelColor(Jimp.cssColorToHex('blue'), x, y);
       }
     }
   }
