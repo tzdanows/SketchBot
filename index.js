a8ff634ad0991f695ea0fd6ccb452537d7edd474
Alex Rodriguez <arodr187@depaul.edu>
2020-10-24 02:00:40 -0500

    added async + clear

--- index.js
+++ index.js
@@ -15,35 +15,53 @@ bot.on('message', msg => {
   if (msg.content.startsWith('!sketch')) {
     var args = (msg.content.trim().split(/ +/g)).slice(0);
     args.shift();
-    draw(args[0], args[1]);
-    setTimeout(function(){ 
-
-      msg.channel.send('Heres the current sketch:', {
-        files:[
-          "resources/templateNew.png"
-        ]
-      });
-  }, 2000); 
-    
+    if (args[0].toUpperCase() == "CLEAR"){
+      clear(msg);
+    }
+    else{
+      draw(args[0], args[1], msg);  
+    }
   } 
 });
 
-async function draw(index, color){
+function draw(index, color, msg){
   const number = index.substring(1);
   const letter = index[0].toUpperCase();
-  Jimp.read('resources/templateNew.png', (err, template) => {
+  Jimp.read('resources/template.png', (err, template) => {
   if (err) throw err;
-  console.log(letter);
   for (var x = numDict[number][0] ; x < numDict[number][1] ; x++){
     for (var y = letterDict[letter][0] ; y < letterDict[letter][1] ; y++){
       template.setPixelColor(Jimp.cssColorToHex(color), x, y);
       }
     }
-    template.write('./resources/templateNew.png'); 
+    template.writeAsync('./resources/template.png', sendMessage(msg));
   });
 }
+
+function clear (msg){
+  Jimp.read('resources/template.png', (err, template) => {
+    if (err) throw err;
+    for (var x = 21 ; x <= 620 ; x ++){
+      for (var y = 21 ; y <= 500 ; y ++){
+        if (x % 20 != 0 && y %20 != 0){
+        template.setPixelColor(Jimp.cssColorToHex('white'), x, y);
+        }
+      }
+    }
+    template.writeAsync('./resources/template.png', sendMessage(msg)); 
+  }); 
+}
+
+function sendMessage(msg){
+  msg.channel.send({
+    files:[
+      "resources/template.png"
+    ]
+  }); 
+}
+
 const letterDict = {
-  A: [21, 40], // dict.A[0] = 21, dict.A[1] = 40
+  A: [21, 40], 
   B: [41, 60],
   C: [61, 80],
   D: [81, 100],
@@ -59,18 +77,19 @@ const letterDict = {
   N: [281, 300],
   O: [301, 320],
   P: [321, 340],
-  Q: [341, 400],
-  R: [401, 420],
-  S: [421, 440],
-  T: [441, 460],
-  U: [461, 480],
-  V: [481, 500],
-  W: [501, 520],
-  X: [521, 540],
-  };
+  Q: [341, 360],
+  R: [361, 380],
+  S: [381, 400],
+  T: [401, 420],
+  U: [421, 440],
+  V: [441, 460],
+  W: [461, 480],
+  X: [481, 500],
+};
+
 
 const numDict = {
-  1: [21, 40], // dict[1][0] = 21, dict[1]][1] = 40
+  1: [21, 40],
   2: [41, 60],
   3: [61, 80],
   4: [81, 100],
@@ -86,30 +105,18 @@ const numDict = {
   14: [281, 300],
   15: [301, 320],
   16: [321, 340],
-  17: [341, 400],
-  18: [401, 420],
-  19: [421, 440],
-  20: [441, 460],
-  21: [461, 480],
-  22: [481, 500],
-  23: [501, 520],
-  24: [521, 540],
-  25: [541, 560],
-  26: [561, 580],
-  27: [581, 600],
-  28: [601, 620],
-  29: [621, 640],
-  30: [641, 660],
+  17: [341, 360],
+  18: [361, 380],
+  19: [381, 400],
+  20: [401, 420],
+  21: [421, 440],
+  22: [441, 460],
+  23: [461, 480],
+  24: [481, 500],
+  25: [501, 520],
+  26: [521, 540],
+  27: [541, 560],
+  28: [561, 580],
+  29: [581, 600],
+  30: [601, 620],
   };
-
-Jimp.read('resources/templateNew.png', (err, template) => {
-  if (err) throw err;
-  for (var x = 21 ; x <= 620 ; x ++){
-    for (var y = 21 ; y <= 500 ; y ++){
-      if (x % 20 != 0 && y %20 != 0){
-      template.setPixelColor(Jimp.cssColorToHex('orange'), x, y);
-      }
-    }
-  }
-  template.write('./resources/templateNew.png'); 
-});
