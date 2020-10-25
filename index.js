199d0236dd9c65f7a96a2f58aa255b8830a81df3
Tomasz Zdanowski <tom.zdan9850@gmail.com>
2020-10-24 23:57:20 -0500

    updated ReadMe and help to reflect new methods

--- index.js
+++ index.js
@@ -6,36 +6,36 @@ const TOKEN = process.env.TOKEN;
 const DATABASE = process.env.DATABASE;
 const data = require('./data');
 const exceptions = require('./exceptions');
-const fs = require('fs');
+const fs = require('fs'); 
 var admin = require("firebase-admin");
 var serviceAccount = require("./serviceKey.json");
 
 //Initialize Firebase
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: DATABASE
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: DATABASE
 });
 
 const db = admin.firestore();
+
 
 //Initialize Discord Bot
 bot.login(TOKEN);
 
 bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}!`);
-  bot.user.setStatus('available')
-  bot.user.setPresence({
-    game: {
-      name: '!sketch help',
-      type: "Streaming🎨",
-      url: "https://www.twitch.tv/bobross"
-    }
-  });
+    bot.user.setStatus('available')
+    bot.user.setPresence({
+        game: {
+            name: '!sketch help',
+            type: "Streaming🎨",
+            url: "https://www.twitch.tv/bobross"
+        }
+    });
 });
 
 bot.on('message', msg => {
   if (msg.content.startsWith('!sketch')) {
-<<<<<<< HEAD
       var args = (msg.content.trim().split(/ +/g)).slice(0);
       args.shift();
       if (checkArgs(args, msg)) {
@@ -65,100 +65,67 @@ bot.on('message', msg => {
           } else {
               draw(args[0], args[1], msg);
           }
-=======
-    var args = (msg.content.trim().split(/ +/g)).slice(0);
-    args.shift();
-    if (checkArgs(args, msg)) {
-      if (args.length == 0) {
-        showSketch(msg);
-      } else if (args[0].toUpperCase() == "CLEAR") {
-        clear(msg);
-      } else if (args[0].toUpperCase() == "HELP") {
-        help(msg);
-      } else if (args[0].toUpperCase() == "LEGEND") {
-        legend(10, msg);
-      } else if (args[0].toUpperCase() == "FILLCANVAS") {
-        fillCanvas(msg);
-      } else if (args[0].toUpperCase() == "IMAGE") {
-        var url;
-        msg.attachments.forEach(attachment => {
-          url = attachment.url;
-        });
-        if (args.length == 1) args.push(url);
-        Jimp.read(args[1]).then(image => {
-            image.resize(30, 24);
-            image.write('temp.jpg', processImage(image, msg));
-          })
-          .catch(err => {
-            msg.reply("The FILE/URL provided does not contain a valid image!");
-          });
-      } else {
-        draw(args[0], args[1], msg);
->>>>>>> 93c43ea4c949bdd1cf20d87cccf193b6e036b9a6
       }
-    }
   }
 });
 
 // SEND CURRENT TEMPLATE
 function sendMessage(msg, name) {
-  msg.channel.send({
-    files: [
-      "resources/" + name
-    ]
-  });
+    msg.channel.send({
+        files: [
+            "resources/" + name
+        ]
+    });
 }
 
 // MAIN COMMAND
 function draw(index, color, msg) {
-  const number = index.substring(1);
-  const letter = index[0].toUpperCase();
+    const number = index.substring(1);
+    const letter = index[0].toUpperCase();
 
-  getImageName(msg).then(name => {
-    if (name == false) {
-      init(index, color, msg);
-      return;
-    } else {
-      Jimp.read('resources/' + name, (err, template) => {
-        if (err) throw err;
-        if (color.toUpperCase() == "RANDOM") {
-          color = randomColor();
-        } else if (Jimp.cssColorToHex(color) == 255 && color != "black" && color != "#000000") {
-          exceptions.invalidColor(msg);
-          return;
+    getImageName(msg).then(name => {
+        if (name == false) {
+            init(index, color, msg);
+            return;
+        } else {
+            Jimp.read('resources/' + name, (err, template) => {
+                if (err) throw err;
+                if (Jimp.cssColorToHex(color) == 255 && color != "black" && color != "#000000") {
+                    exceptions.invalidColor(msg);
+                    return;
+                }
+                for (var x = data.numDict[number][0]; x < data.numDict[number][1]; x++) {
+                    for (var y = data.letterDict[letter][0]; y < data.letterDict[letter][1]; y++) {
+                        template.setPixelColor(Jimp.cssColorToHex(color), x, y);
+                    }
+                }
+                template.writeAsync('./resources/' + name, sendMessage(msg, name));
+            });
         }
-        for (var x = data.numDict[number][0]; x < data.numDict[number][1]; x++) {
-          for (var y = data.letterDict[letter][0]; y < data.letterDict[letter][1]; y++) {
-            template.setPixelColor(Jimp.cssColorToHex(color), x, y);
-          }
-        }
-        template.writeAsync('./resources/' + name, sendMessage(msg, name));
-      });
-    }
-  });
+    });
 }
 
 function clear(msg) {
-  getImageName(msg).then(name => {
-    if (name == false) {
-      msg.reply("Please sketch a bit before clearing!");
-      return;
-    } else {
-      Jimp.read('resources/' + name, (err, template) => {
-        if (err) throw err;
-        for (var x = 21; x <= 620; x++) {
-          for (var y = 21; y <= 500; y++) {
-            if (x % 20 != 0 && y % 20 != 0) {
-              template.setPixelColor(Jimp.cssColorToHex('white'), x, y);
-            }
-          }
+    getImageName(msg).then(name => {
+        if (name == false) {
+            msg.reply("Please sketch a bit before clearing!");
+            return;
+        } else {
+            Jimp.read('resources/' + name, (err, template) => {
+                if (err) throw err;
+                for (var x = 21; x <= 620; x++) {
+                    for (var y = 21; y <= 500; y++) {
+                        if (x % 20 != 0 && y % 20 != 0) {
+                            template.setPixelColor(Jimp.cssColorToHex('white'), x, y);
+                        }
+                    }
+                }
+                template.writeAsync('./resources/' + name, sendMessage(msg, name));
+
+            });
+
         }
-        template.writeAsync('./resources/' + name, sendMessage(msg, name));
-
-      });
-
-    }
-  });
+    });
 }
 
 function help(msg) {
@@ -187,7 +154,7 @@ function help(msg) {
                   },
                   {
                     name: "**image [upload]**",
-                    value: "> Resizes a given image via discord upload to the canvas size and shifts each canvas cell to the most applicable color. \n !sketch {upload a file here} "
+                    value: "> Resizes a given image via discord upload to the canvas size and shifts each canvas cell to the most applicable color. \n `!sketch {image file upload}` `!sketch {upload a file here}` "
                 },
                   {
                     name: "**fillcanvas []**",
@@ -208,109 +175,108 @@ function help(msg) {
                 text: "Team ATA - Demonhacks 2020"
             }
         }
-    }
-  );
+    });
 }
 
 function processImage(image, msg) {
 
-  getImageName(msg).then(name => {
-    if (name == false) {
-      msg.reply("Please draw BEFORE replacing an image");
-      return;
-    } else {
-      Jimp.read('resources/' + name, (err, template) => {
-        if (err) throw err;
-        var xblockcounter = 1;
-        var yblockcounter = 1;
-        var blocksize = 1;
-        for (var x = 21; x <= 620; x++) {
-          for (var y = 21; y <= 500; y++) {
-            if (x % 20 != 0 && y % 20 != 0) {
-              var color = image.getPixelColor(xblockcounter, yblockcounter);
-              template.setPixelColor(color, x, y);
-            } else {
-              yblockcounter++;
-              if (blocksize > 20) {
-                xblockcounter++;
-                blocksize = 1;
-              }
-            }
-          }
-          yblockcounter = 1;
-          blocksize++;
+    getImageName(msg).then(name => {
+        if (name == false) {
+            msg.reply("Please draw BEFORE replacing an image");
+            return;
+        } else {
+            Jimp.read('resources/' + name, (err, template) => {
+                if (err) throw err;
+                var xblockcounter = 1;
+                var yblockcounter = 1;
+                var blocksize = 1;
+                for (var x = 21; x <= 620; x++) {
+                    for (var y = 21; y <= 500; y++) {
+                        if (x % 20 != 0 && y % 20 != 0) {
+                            var color = image.getPixelColor(xblockcounter, yblockcounter);
+                            template.setPixelColor(color, x, y);
+                        } else {
+                            yblockcounter++;
+                            if (blocksize > 20) {
+                                xblockcounter++;
+                                blocksize = 1;
+                            }
+                        }
+                    }
+                    yblockcounter = 1;
+                    blocksize++;
+                }
+                template.writeAsync('./resources/' + name, sendMessage(msg, name));
+            });
+          if (fs.existsSync('./temp.jpg')){
+            fs.unlink('./temp.jpg', (err => {
+              if (err)
+                  console.log(err);
+            }));
+          } 
         }
-        template.writeAsync('./resources/' + name, sendMessage(msg, name));
-      });
-      if (fs.existsSync('./temp.jpg')) {
-        fs.unlink('./temp.jpg', (err => {
-          if (err)
-            console.log(err);
-        }));
-      }
-    }
-  });
+    });
 
 }
 
 function legend(inter, msg) {
-  num = getRandomInt(inter);
-  msg.reply(data.legendDict[num][0]);
+    num = getRandomInt(inter);
+    msg.reply(data.legendDict[num][0]);
 }
 
 // ARGUMENT CHECKING
 function checkArgs(args, msg) {
-  if (args.length > 2) {
-    exceptions.invalidMsg(msg)
-    return false;
-  }
-
-  if (args.length == 0) {
-    return true;
-  }
-  if (args[0].toUpperCase() == "CLEAR" && args.length == 1) {
-    return true;
-  }
-  if (args[0].toUpperCase() == "FILLCANVAS" && args.length == 1) {
-    return true;
-  }
-  if (args[0].toUpperCase() == "HELP" && args.length == 1) {
-    return true;
-  }
-  if (args[0].toUpperCase() == "LEGEND" && args.length == 1) {
-    return true;
-  }
-
-  if (args[0].toUpperCase() == "IMAGE" && args.length <= 2) {
-    return true;
-  }
-
-  try {
-    number = args[0].substring(1);
-    letter = args[0][0].toUpperCase();
-    if (number in data.numDict && letter in data.letterDict && args.length == 2) {
-      return true;
-    } else {
-      exceptions.invalidCoord(msg)
-      return false;
+    if (args.length > 2) {
+        exceptions.invalidMsg(msg)
+        return false;
     }
-  } catch {
 
-    exceptions.invalidMsg(msg)
-    return false;
+    if (args.length == 0){
+        return true;
+    }
+    if (args[0].toUpperCase() == "CLEAR" && args.length == 1) {
+        return true;
+    }
+    if (args[0].toUpperCase() == "FILLCANVAS" && args.length == 1) {
+      return true;
+    }
+    if (args[0].toUpperCase() == "HELP" && args.length == 1) {
+        return true;
+    }
+    if (args[0].toUpperCase() == "LEGEND" && args.length == 1) {
+        return true;
+    }
+
+    if (args[0].toUpperCase() == "IMAGE" && args.length <= 2) {
+      return true;
   }
+
+    try {
+        number = args[0].substring(1);
+        letter = args[0][0].toUpperCase();
+        if (number in data.numDict && letter in data.letterDict && args.length == 2) {
+            return true;
+        } else {
+            exceptions.invalidCoord(msg)
+            return false;
+        }
+    } catch {
+        
+        exceptions.invalidMsg(msg)
+        return false;
+    }
 }
 
 // Helper methods
 function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+    return Math.floor(Math.random() * Math.floor(max));
 }
 
-function fillCanvas(msg) {
+function fillCanvas(msg){
   getImageName(msg).then(name => {
     if (name == true) {
-      msg.reply("Can only when the canvas is empty! Try !sketch clear()");
-      return;
+        msg.reply("Can only when the canvas is empty! Try !sketch clear()");
+        return;
     } else {
       Jimp.read('resources/' + name, (err, template) => {
         if (err) throw err;
@@ -321,72 +287,74 @@ function fillCanvas(msg) {
         var color = randomColor();
         color[1] = randomColor();
         for (var x = 21; x <= 620; x++) {
-          for (var y = 21; y <= 500; y++) {
-            if (x % 20 != 0 && y % 20 != 0) {
-              template.setPixelColor(Jimp.cssColorToHex(cells[yblockcounter]), x, y);
-
-            } else {
-              cells[yblockcounter] = color;
-              yblockcounter++;
-              color = randomColor();
-              if (blocksize > 20) {
-                xblockcounter++;
-                cells = Array(24);
-                color[1] = randomColor();
-                blocksize = 1;
-              }
+            for (var y = 21; y <= 500; y++) {
+                if (x % 20 != 0 && y % 20 != 0) {
+                    template.setPixelColor(Jimp.cssColorToHex(cells[yblockcounter]), x, y);
+                    
+                } else {
+                    cells[yblockcounter] = color;
+                    yblockcounter++;
+                    color = randomColor();
+                    if (blocksize > 20) {
+                        xblockcounter++;
+                        cells = Array(24);
+                        color[1] = randomColor();
+                        blocksize = 1;
+                    }
+                }
             }
-          }
-          yblockcounter = 1;
-          blocksize++;
-          color = randomColor();
+            yblockcounter = 1;
+            blocksize++;
+            color = randomColor();
         }
         template.writeAsync('./resources/' + name, sendMessage(msg, name));
-      });
+    });
+
     }
-  })
+})
 }
 
-function randomColor() {
-  var randColor = Math.floor(Math.random() * 16777215).toString(16);
+function randomColor(){
+  var randColor = Math.floor(Math.random()*16777215).toString(16);
   return randColor;
 }
+console.log(randomColor());
 
-function showSketch(msg) {
-  getImageName(msg).then(name => {
-    if (name == false) {
-      msg.reply("Please sketch a bit first! Run **!sketch help** for help.");
-      return;
-    } else {
-      sendMessage(msg, name);
-    }
-  });
+function showSketch(msg){
+    getImageName(msg).then(name => {
+        if (name == false) {
+            msg.reply("Please sketch a bit first! Run **!sketch help** for help.");
+            return;
+        } else {
+            sendMessage(msg, name);
+        }
+    });
 }
 
 // CLOUD FIREBASE METHODS
 function init(index, color, msg) {
-  Jimp.read('resources/default.png', (err, template) => {
-    if (err) throw err;
-    const name = msg.guild.id + ".png";
-    template.writeAsync('./resources/' + name, draw(index, color, msg));
-    writeDb(msg);
-  });
+    Jimp.read('resources/default.png', (err, template) => {
+        if (err) throw err;
+        const name = msg.guild.id + ".png";
+        template.writeAsync('./resources/' + name, draw(index, color, msg));
+        writeDb(msg);
+    });
 }
 
 function writeDb(msg) {
-  const hashData = {
-    imageHash: msg.guild.id
-  };
-  db.collection('serverData').doc(msg.guild.id).set(hashData);
+    const hashData = {
+        imageHash: msg.guild.id
+    };
+    db.collection('serverData').doc(msg.guild.id).set(hashData);
 }
 
 async function getImageName(msg) {
 
-  const info = db.collection('serverData').doc(msg.guild.id)
-  const doc = await info.get();
-  if (!doc.exists) {
-    return false;
-  } else {
-    return (doc._fieldsProto.imageHash.stringValue + ".png");
-  }
+    const info = db.collection('serverData').doc(msg.guild.id)
+    const doc = await info.get();
+    if (!doc.exists) {
+        return false;
+    } else {
+        return (doc._fieldsProto.imageHash.stringValue + ".png");
+    }
 }
